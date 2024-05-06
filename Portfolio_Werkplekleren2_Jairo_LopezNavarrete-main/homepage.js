653eea87a8b1472572243eea1f6d475616b9e488
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2024-05-06 23:30:39 +0200

    e

--- Portfolio_Werkplekleren2_Jairo_LopezNavarrete-main/homepage.js
+++ Portfolio_Werkplekleren2_Jairo_LopezNavarrete-main/homepage.js
@@ -3,10 +3,24 @@ icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
 }*/
 
-let icon = document.getElementById("icon");
-icon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
+const isDarkMode = localStorage.getItem("darkMode") === "true";
+
+// Stel de donkere modus in op basis van de opgeslagen waarde
+if (isDarkMode) {
+    document.body.classList.add("dark-theme");
 }
+
+// Selecteer de dark mode knop
+const icon = document.getElementById("icon");
+
+// Wissel tussen donkere en lichte modus bij het klikken op de knop
+icon.addEventListener("click", function() {
+    document.body.classList.toggle("dark-theme");
+
+    // Bewaar de huidige staat van de donkere modus in de localStorage
+    const isDarkModeEnabled = document.body.classList.contains("dark-theme");
+    localStorage.setItem("darkMode", isDarkModeEnabled);
+});
 
 /*function myFunction() {
     var element = document.body;
